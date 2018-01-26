f0250ef25d79e834c110b086d3ded31792564e22
Charfire <charfiremaster@gmail.com>
2018-01-26 23:12:12 +0100

    Camera , start en game over
    Een werkende camera, een startscherm en een game over scherm.

---
+++ Game jam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    private float cameraSpeed = 5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ScrollUp(cameraSpeed);
+	}
+
+    private void ScrollUp(float speed)
+    {
+        Vector3 newLocation = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime, transform.position.z);
+        transform.position = newLocation;
+    }
+}
