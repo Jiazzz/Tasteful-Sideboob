4362f213e0a595da87aa2fa97f76e3fa9e19706f
PandaPal <liam.rougoor@gmail.com>
2018-01-27 12:20:59 +0100

    Cutscene Update
    Cutscene Manager, Cutscene Assets, nog niet helemaal af

--- Game jam/Assets/Scripts/CutsceneManager.cs
+++ Game jam/Assets/Scripts/CutsceneManager.cs
@@ -1,16 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CutsceneManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    Cutscene[] cutscenes;
+    int currentCutscene;
+    GameObject[] cutsceneImages;
+    int currentImage;
+
+    Image image;
+
+    // Use this for initialization
+    void Start()
+    {
+        image = GetComponent<Image>();
+        currentCutscene = 0;
+    }
+
+    private void OnEnable()
+    {
+        cutsceneImages = cutscenes[currentCutscene].cutsceneImages;
+        currentImage = 0;
+        image.sprite = cutsceneImages[currentImage].GetComponent<SpriteRenderer>().sprite;
+        image.color = cutsceneImages[currentImage].GetComponent<SpriteRenderer>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            nextImage();
+        }
+    }
+
+    void nextImage()
+    {
+        if (currentImage < cutsceneImages.Length - 1)
+        {
+            currentImage++;
+            image.sprite = cutsceneImages[currentImage].GetComponent<SpriteRenderer>().sprite;
+            image.color = cutsceneImages[currentImage].GetComponent<SpriteRenderer>().color;
+        }
+        else
+        {
+            EndCutscene();
+        }
+    }
+
+    void EndCutscene()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void NextCutscene()
+    {
+        if (currentCutscene < cutscenes.Length - 1)
+        {
+            currentCutscene++;
+        }
+    }
 }
