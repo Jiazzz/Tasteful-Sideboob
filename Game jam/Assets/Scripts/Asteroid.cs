e584ad27fb94417cc571d8549f5d7807a824b6bb
PandaPal <liam.rougoor@gmail.com>
2018-01-27 15:22:09 +0100

    Asteroids
    Asteroid collisions

--- Game jam/Assets/Scripts/Asteroid.cs
+++ Game jam/Assets/Scripts/Asteroid.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour {
 
-    float speed = 2;
-	int damage = 20; 
-	int hp = 40; 
+    [SerializeField]
+    protected float speed = 2;
+    [SerializeField]
+	protected int damage = 20; 
+    [SerializeField]
+	protected int hp = 40; 
 	
-	[SerializeField]
-    GameObject subAsteroidPrefab;
-	
-	//TODO: collision with bullet, hp -= amount
-
-
 	// Update is called once per frame
     void Update() {
         if (hp <= 0)
@@ -21,24 +18,25 @@ public class Asteroid : MonoBehaviour {
             Break();
         }
 	}
-	
-	void LateUpdate () {
-        transform.Translate(transform.down * speed * Time.deltaTime);
-	}
-	
-	void Break()
-    {
-        bool diag = rand.NextDouble() > 0.5;
-		EjectAsteroid(diag, 0);
-		EjectAsteroid(diag, 90);
-		EjectAsteroid(diag, 180);
-		EjectAsteroid(diag, 270);
-    }
-	
-	void EjectAsteroid(bool diag, int angle)
-	{
-		GameObject subAsteroid = Instantiate(subAsteroidPrefab, transform.position, Quaternion.eulerAngles(angle + diag * 45));
-	}
 
-    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        TakeDamage(10);
+    }
+
+    void LateUpdate () {
+        transform.Translate(transform.up * -speed * Time.deltaTime, Space.World);
+	}
+	
+
+    void TakeDamage(int amount)
+    {
+        hp -= amount;
+    }
+
+    protected virtual void Break()
+    {
+        Destroy(gameObject);
+    }
+ 
 }
---
+++ Game jam/Assets/Scripts/BigAsteroid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigAsteroid : Asteroid {
+
+    [SerializeField]
+    GameObject subAsteroidPrefab;
+
+    protected override void Break()
+    {
+        EjectAsteroid(0);
+        EjectAsteroid(90);
+        EjectAsteroid(180);
+        EjectAsteroid(270);
+        Destroy(gameObject);
+    }
+
+    void EjectAsteroid(int angle)
+    {
+        Quaternion rotation = Quaternion.identity;
+        float factor = Random.Range(0, 5);
+        rotation.eulerAngles = new Vector3(0, 0, angle + factor/5 * 45);
+        GameObject subAsteroid = Instantiate(subAsteroidPrefab, transform.position, rotation);
+    }
+}
